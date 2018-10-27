5b6818809f3cce3c39bdfab592c1dd425d19ce9b
Jabasilva <francis.gallaxy@gmail.com>
2018-10-27 19:16:25 +0530

    Student API Will provide basic information about student Name : Jabasilva Francis S

---
+++ StudentAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,81 @@
+﻿using StudentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        Dictionary<string, string> emp = new Dictionary<string, string>();
+
+      
+
+        // GET: api/Employee
+        public IEnumerable<Employee> Get()
+        {
+
+            return EmployeeDataPopulator();
+        }
+
+        internal static IEnumerable<Employee> EmployeeDataPopulator()
+        {
+            IEnumerable<Employee> empData;
+
+            List<Employee> data = new List<Employee>();
+            for(int i=0;i<=20;i++)
+            {
+                Employee empIn = new Employee();
+                empIn.ID = i + 1;
+                empIn.Name = "Demo "+i;
+                empIn.Mobile = "99998880" + i;
+                empIn.Qualification = i+ " Class";
+                if (i % 2 == 0)
+                {
+                    empIn.Gender = "Male";
+                    empIn.Address = "Chennai";
+                }
+                else
+                {
+                    empIn.Gender = "Female";
+                    empIn.Address = "Bangalore";
+                }
+                empIn.IsActive = true;
+
+                data.Add(empIn);
+            }
+
+            empData = data.ToList();
+            
+
+            return empData.ToList();
+        }
+
+        // GET: api/Employee/5
+        public IEnumerable<Employee> Get(int id)
+        {
+            
+            IEnumerable<Employee> empData = EmployeeDataPopulator();
+
+            IEnumerable <Employee> empResult = empData.Where( x => x.ID == id).ToList();
+            return empResult;
+        }
+        // POST: api/Employee
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Employee/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Employee/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ StudentAPI/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAPI.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Qualification { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
